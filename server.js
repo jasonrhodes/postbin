b3c03e83c40e715d57830c19b7249b4eb4a62ed9
Jason Rhodes <jason.matthew.rhodes@gmail.com>
2015-08-20 16:23:14 -0400

    Preparing for initial npm publish

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ cluster(function() {
   var app = express();
 
   app.use(bodyParser.json({limit: '50mb', strict:false}));
-
   app.use(bearerToken());
 
   app.all('/', function (req, res) {
@@ -35,14 +34,18 @@ cluster(function() {
   });
 
   app.all('/logged/:statusCode*', authTimeout, function (req, res) {
-    var body = req.body
-      , authTimeout = req.query.authTimeout;
+    var body = req.body;
+
     if (Array.isArray(body)) {
       console.log("got an array of length: " + body.length);
     } else {
       console.log("got an object: " + body);
     }
-    console.log(JSON.stringify(req.headers));
+    console.log('headers:\n', JSON.stringify(req.headers));
+
+    if (req.token) {
+      console.log('Bearer token:', req.token);
+    }
 
     res.status(req.params.statusCode).send({});
   });
@@ -53,24 +56,6 @@ cluster(function() {
       console.error('bad auth: ' + req.headers['x-messagesystems-webhook-token']);
     }
     res.status(authed ? 200 : 401).send({});
-  });
-
-  app.all('/headers', function (req, res) {
-    var headers = req.headers
-      , authTimeout = req.query.authTimeout
-      , token = req.token;
-
-    console.log("got a header: " + JSON.stringify(headers));
-    console.log('token', token);
-
-    if (authTimeout) {
-      console.log(new Date().getTime() - token);
-      if(!token || new Date().getTime() - token < authTimeout) {
-        res.status(401).send();
-        return;
-      }
-    }
-    res.status(200).send({});
   });
 
   app.all('/slow/:averageTime*', function (req, res) {
@@ -93,7 +78,7 @@ cluster(function() {
 
     console.log(new Date(), 'token endpoint called', req.body);
     
-    switch(req.body.grant_type) {
+    switch (req.body.grant_type) {
       case 'client_credentials':
         var expires_in = req.query.expires_in
           , token_type = req.query.token_type
@@ -120,25 +105,19 @@ cluster(function() {
 }, { count: spawnCount });
 
 
-function getLogger(req) {
-  return function() {
-    if (req.url.match(/^\/logged\//)) {
-      console.log.apply(console, arguments);
-    }
-  }
-}
-
+/**
+ * Middleware for checking timestamp token
+ * against authTimeout ms value, and responding
+ * with 401 if token is older than authTimeout ms ago
+ */
 function authTimeout(req, res, next) {
   var timeout = req.query.authTimeout
-    , token = req.token
-    , log = getLogger(req);
+    , token = req.token;
   
   if (!timeout) {
     return next();
   }
 
-  var token = req.token;
-  log('token', token);
   if(!token || new Date().getTime() - token > timeout) {
     res.status(401).send({
       error: 'invalid or expired token: ' + token
