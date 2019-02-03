49fb3dcdf314fb33d9ca4a098fdcc018aef7536e
Jason Rhodes <jason.matthew.rhodes@gmail.com>
2019-02-03 12:21:38 -0500

    Updated readme and index view

--- README.md
+++ README.md
@@ -1,52 +1,49 @@
 # Postbin
 
-## Group Installation
+Simple set of endpoints for mocking out HTTP calls. See below for calls you can make.
 
-You will need NodeJS installed to use this project. Once you've installed NodeJS and NPM successfully, install the postbin's dependencies by changing to the postbin directory and running `npm install`.
+Visit https://github.com/jasonrhodes/postbin for this code.
 
-To run, use `npm start`, or to keep it running in the background, use `npm run forever:start` and `npm run forever:stop` to stop later.
-
-## Group Endpoints
+## Endpoints
 
 ### GET /status/{statusCode}
 
 Returns an HTTP response with the given status code.
 
-+ Parameters
-  + statusCode (required, integer, `200`) ... The response status to return.
+- Parameters
+  - statusCode (required, integer, `200`) ... The response status to return.
 
 ### GET /wildcard/{chance}
 
 Returns an HTTP response whose status code is either 200 or 500, with the chance of success determined by the `chance` parameter.
 
-+ Parameters
-  + chance (required, integer, `2`) ... The HTTP request will succeed (return status 200) about one time in this many; for example, with a value of 2, approximately half the requests will succeed.
+- Parameters
+  - chance (required, integer, `2`) ... The HTTP request will succeed (return status 200) about one time in this many; for example, with a value of 2, approximately half the requests will succeed.
 
 ### GET /auth/{authCode}
 
 Returns an HTTP response whose status code is either 200 or 400, depending on whether the given `authCode` matches the value of the request's `X-MessageSystems-Webhook-Token` header.
 
-+ Parameters
-  + authCode (required, string, `foo`) ... The authentication token value to compare against the request's `X-MessageSystems-Webhook-Token` header.
+- Parameters
+  - authCode (required, string, `foo`) ... The authentication token value to compare against the request's `X-MessageSystems-Webhook-Token` header.
 
 ### GET /logged/{statusCode}
 
 Returns an HTTP response with the given status code, and also logs the request body to the console.
 
-+ Parameters
-  + statusCode (required, integer, `200`) ... The response status to return.
+- Parameters
+  - statusCode (required, integer, `200`) ... The response status to return.
 
 ### GET /slow/{averageTime}
 
 Returns an HTTP response with status 200, after waiting between 0 and `averageTime * 2` milliseconds.
 
-+ Parameters
-  + averageTime (required, integer, `1000`) ... The average number of milliseconds to wait before returning a response.
+- Parameters
+  - averageTime (required, integer, `1000`) ... The average number of milliseconds to wait before returning a response.
 
 ### GET /delay/{delayTime}
 
 Returns an HTTP response with status 200, after waiting exactly `delayTime` milliseconds.
 
-+ Parameters
-  + delay (required, integer, `1000`) ... The number of milliseconds to wait before returning a response.
-
+- Parameters
+  - delay (required, integer, `1000`) ... The number of milliseconds to wait before returning a response.
--- server.js
+++ server.js
@@ -1,83 +1,115 @@
-var express = require('express')
-  , cluster = require('express-cluster')
-  , bodyParser = require('body-parser')
-  , bearerToken = require('express-bearer-token')
-  , _ = require('lodash')
-  , crypto = require('crypto')
-  , port = process.env.POSTBIN_TOKEN_PORT || process.env.POSTBIN_PORT || process.env.PORT || 4000
-  , spawnCount = process.env.POSTBIN_SPAWN_COUNT || 1;
+const express = require("express"),
+  cluster = require("express-cluster"),
+  bodyParser = require("body-parser"),
+  bearerToken = require("express-bearer-token"),
+  _ = require("lodash"),
+  crypto = require("crypto"),
+  port =
+    process.env.POSTBIN_TOKEN_PORT ||
+    process.env.POSTBIN_PORT ||
+    process.env.PORT ||
+    4000,
+  spawnCount = process.env.POSTBIN_SPAWN_COUNT || 1;
 
-cluster(function() {
-  var app = express();
+const Markdown = require("markdown-to-html").Markdown;
+const md = new Markdown();
 
-  app.use('/token', bodyParser.urlencoded({ extended: false }));
-  app.post('/token', logHeader, delayResponse, wildcard, tokenEndpoint);
+cluster(
+  function() {
+    const app = express();
 
-  app.use(bearerToken());
-  app.use(bodyParser.json({ limit: '50mb', strict: false }));
+    app.use("/token", bodyParser.urlencoded({ extended: false }));
+    app.post("/token", logHeader, delayResponse, wildcard, tokenEndpoint);
 
-  app.all('/', function (req, res) {
-    var delay = req.headers['x-delay'] || null;
-    var statusCode = req.headers['x-status'] || 200;
+    app.use(bearerToken());
+    app.use(bodyParser.json({ limit: "50mb", strict: false }));
 
-    if (delay) {
-      setTimeout(function () {
+    app.get("/", (req, res) => {
+      md.render(
+        __dirname + "/README.md",
+        { stylesheet: "assets/readme.css" },
+        err => {
+          if (err) {
+            res.send("Error parsing README markdown");
+          }
+          md.pipe(res);
+        }
+      );
+    });
+
+    app.use("/assets", express.static("static"));
+
+    app.all("/", function(req, res) {
+      const delay = req.headers["x-delay"] || null;
+      const statusCode = req.headers["x-status"] || 200;
+
+      if (delay) {
+        setTimeout(function() {
+          res.status(statusCode).send({});
+        }, delay);
+      } else {
         res.status(statusCode).send({});
-      }, delay);
-    } else {
-      res.status(statusCode).send({});
-    }
-  });
+      }
+    });
 
-  app.all('/wildcard/:freq*', function (req, res) {
-    var failed = (Math.random() * req.params.freq < 1);
-    res.status(failed ? 500 : 200).send({});
-  });
+    app.all("/wildcard/:freq*", function(req, res) {
+      const failed = Math.random() * req.params.freq < 1;
+      res.status(failed ? 500 : 200).send({});
+    });
 
-  app.all('/status/:statusCode*', delayResponse, function (req, res) {
-    res.status(req.params.statusCode).send({});
-  });
+    app.all("/status/:statusCode*", delayResponse, function(req, res) {
+      res.status(req.params.statusCode).send({});
+    });
 
-  app.all('/logged/:statusCode*', logHeader, delayResponse, loggedEndpoint);
+    app.all("/logged/:statusCode*", logHeader, delayResponse, loggedEndpoint);
 
-  app.all('/auth/:authCode*', function (req, res) {
-    var authed = req.headers['x-messagesystems-webhook-token'] === req.params.authCode;
-    if (!authed) {
-      console.error('bad auth: ' + req.headers['x-messagesystems-webhook-token']);
-    }
-    res.status(authed ? 200 : 401).send({});
-  });
+    app.all("/auth/:authCode*", function(req, res) {
+      const authed =
+        req.headers["x-messagesystems-webhook-token"] === req.params.authCode;
+      if (!authed) {
+        console.error(
+          "bad auth: " + req.headers["x-messagesystems-webhook-token"]
+        );
+      }
+      res.status(authed ? 200 : 401).send({});
+    });
 
-  app.all('/slow/:averageTime*', function (req, res) {
-    var time = Math.floor(Math.random() * req.params.averageTime * 2);
-    var msg = 'Waited ' + time + 'ms';
-    msg = { message: msg };
+    app.all("/slow/:averageTime*", function(req, res) {
+      const time = Math.floor(Math.random() * req.params.averageTime * 2);
+      const msg = "Waited " + time + "ms";
+      msg = { message: msg };
 
-    setTimeout(function () {
-      res.status(200).send(msg);
-    }, time);
-  });
+      setTimeout(function() {
+        res.status(200).send(msg);
+      }, time);
+    });
 
-  app.all('/delay/:delayTime*', function (req, res) {
-    setTimeout(function () {
-      res.status(200).send({});
-    }, req.params.delayTime);
-  });
+    app.all("/delay/:delayTime*", function(req, res) {
+      setTimeout(function() {
+        res.status(200).send({});
+      }, req.params.delayTime);
+    });
 
-  app.all('/logged', logHeader, delayResponse, wildcard, authTimeout, loggedEndpoint);
+    app.all(
+      "/logged",
+      logHeader,
+      delayResponse,
+      wildcard,
+      authTimeout,
+      loggedEndpoint
+    );
 
-  app.use(function(err, req, res, next) {
-    console.error(err.stack);
-    res.status(500).send('Something broke!');
-  });
+    app.use(function(err, req, res, next) {
+      console.error(err.stack);
+      res.status(500).send("Something broke!");
+    });
 
-  app.listen(port, function () {
-    console.log('Express server listening on port ' + port);
-  });
-}, { count: spawnCount });
-
-
-
+    app.listen(port, function() {
+      console.log("Express server listening on port " + port);
+    });
+  },
+  { count: spawnCount }
+);
 
 // Endpoint functions
 
@@ -86,10 +118,10 @@ cluster(function() {
  * @param req
  * @param res
  */
-function loggedEndpoint (req, res) {
-  var body = req.body;
+function loggedEndpoint(req, res) {
+  const body = req.body;
 
-  console.log('Headers: ' + JSON.stringify(req.headers));
+  console.log("Headers: " + JSON.stringify(req.headers));
   if (Array.isArray(body)) {
     console.log("Body is an array of length", body.length);
   } else {
@@ -106,34 +138,37 @@ function loggedEndpoint (req, res) {
  * @param res
  */
 function tokenEndpoint(req, res) {
-
-  var contentType = req.headers['content-type'];
+  const contentType = req.headers["content-type"];
 
   if (!contentType.match(/application\/x-www-form-urlencoded/)) {
-    console.log('Invalid content type of ' + contentType + ', responding with 415');
-    return res.status(415).send('Please submit data in "x-www-form-urlencoded" format only');
+    console.log(
+      "Invalid content type of " + contentType + ", responding with 415"
+    );
+    return res
+      .status(415)
+      .send('Please submit data in "x-www-form-urlencoded" format only');
   }
 
   switch (req.body.grant_type) {
-    case 'client_credentials':
-      var response = _.pick(req.body, 'expires_in', 'refresh_token');
+    case "client_credentials":
+      const response = _.pick(req.body, "expires_in", "refresh_token");
       response.access_token = new Date().getTime().toString();
 
-      console.log('Valid token generated: ' + response.access_token);
+      console.log("Valid token generated: " + response.access_token);
       res.status(200).send(response);
       break;
 
     default:
-      console.log('Invalid grant_type in token request body: ' + req.body.grant_type);
+      console.log(
+        "Invalid grant_type in token request body: " + req.body.grant_type
+      );
       res.status(400).send({
-        error: 'invalid grant type'
+        error: "invalid grant type"
       });
   }
 
-  console.log('Body: ' + JSON.stringify(req.body));
+  console.log("Body: " + JSON.stringify(req.body));
 }
-
-
 
 // Middleware functions
 
@@ -143,35 +178,42 @@ function tokenEndpoint(req, res) {
  * with 401 if token is older than authTimeout ms ago
  */
 function authTimeout(req, res, next) {
-  var timeout = req.query.authTimeout
-    , token = req.token
-    , elapsed = new Date().getTime() - token;
+  const timeout = req.query.authTimeout,
+    token = req.token,
+    elapsed = new Date().getTime() - token;
 
   if (!timeout) {
     return next();
   }
 
-  if (!_.isUndefined(req.headers['x-messagesystems-batch-id'])) {
-    console.log('x-messagesystems-batch-id:', req.headers['x-messagesystems-batch-id']);
+  if (!_.isUndefined(req.headers["x-messagesystems-batch-id"])) {
+    console.log(
+      "x-messagesystems-batch-id:",
+      req.headers["x-messagesystems-batch-id"]
+    );
   }
 
   if (!token) {
-    console.log('No token included in request, responding with 400');
+    console.log("No token included in request, responding with 400");
     res.status(400).send({
-      error: 'no access_token'
+      error: "no access_token"
     });
     return;
   }
 
   if (elapsed > timeout) {
-    console.log('Invalid or expired token %s, responding with 401', token);
+    console.log("Invalid or expired token %s, responding with 401", token);
     res.status(401).send({
-      error: 'invalid or expired token: ' + token
+      error: "invalid or expired token: " + token
     });
     return;
   }
 
-  console.log('%s ms remaining before token %s expires', (timeout - elapsed), token);
+  console.log(
+    "%s ms remaining before token %s expires",
+    timeout - elapsed,
+    token
+  );
   next();
 }
 
@@ -185,22 +227,22 @@ function authTimeout(req, res, next) {
  * @returns {*}
  */
 function wildcard(req, res, next) {
-var random = Math.random()
-  , badResponses = {}
-  , statusCode;
+  const random = Math.random(),
+    badResponses = {};
+  let statusCode;
 
   if (!req.query.badResponses) {
     return next();
   }
 
   try {
-    var codes = req.query.badResponses.split(',');
+    const codes = req.query.badResponses.split(",");
     codes.forEach(function(item) {
-      var c = item.split(':');
+      const c = item.split(":");
       badResponses[c[0]] = parseFloat(c[1]);
     });
   } catch (e) {
-    console.log('could not parse badResponses: ' + req.query.badResponses);
+    console.log("could not parse badResponses: " + req.query.badResponses);
     next();
   }
 
@@ -208,16 +250,20 @@ var random = Math.random()
     return next();
   }
 
-  _.reduce(badResponses, function(totalPercentage, percentage, code) {
-    totalPercentage += percentage;
-    if (!statusCode && random <= totalPercentage) {
-      statusCode = code;
-    }
-    return totalPercentage;
-  }, 0);
+  _.reduce(
+    badResponses,
+    function(totalPercentage, percentage, code) {
+      totalPercentage += percentage;
+      if (!statusCode && random <= totalPercentage) {
+        statusCode = code;
+      }
+      return totalPercentage;
+    },
+    0
+  );
 
   if (statusCode) {
-    console.log('Bad response womp womp, responding with %s', statusCode);
+    console.log("Bad response womp womp, responding with %s", statusCode);
     res.status(statusCode).send();
     return;
   }
@@ -232,7 +278,9 @@ var random = Math.random()
  */
 function logHeader(req, res, next) {
   req.hash = md5(req.url + req.ip).substr(0, 6);
-  console.log('\n' + timestamp() + ' | ' + req.hash + ' | ' + req.ip + ' | ' + req.url);
+  console.log(
+    "\n" + timestamp() + " | " + req.hash + " | " + req.ip + " | " + req.url
+  );
   next();
 }
 
@@ -244,17 +292,18 @@ function logHeader(req, res, next) {
  *
  */
 function delayResponse(req, res, next) {
-  var delay = req.query.delay;
+  const delay = req.query.delay;
   if (!delay) {
     return next();
   }
-  var ms, range = delay.split('-');
+  let ms;
+  const range = delay.split("-");
   if (range.length === 1) {
     ms = delay;
   } else {
     ms = _.random.apply(_, range);
   }
-  console.log('Delay for ' + ms + 'ms');
+  console.log("Delay for " + ms + "ms");
   setTimeout(next, ms);
 }
 
@@ -263,8 +312,15 @@ function delayResponse(req, res, next) {
  * @return {[type]} [description]
  */
 function timestamp() {
-  var d = new Date();
-  return d.toTimeString().split(' ').shift() + ' ' + d.toDateString();
+  const d = new Date();
+  return (
+    d
+      .toTimeString()
+      .split(" ")
+      .shift() +
+    " " +
+    d.toDateString()
+  );
 }
 
 /**
@@ -273,7 +329,7 @@ function timestamp() {
  * @return {String}       Hashed value
  */
 function md5(value) {
-  var md5sum = crypto.createHash('md5');
+  const md5sum = crypto.createHash("md5");
   md5sum.update(value);
-  return md5sum.digest('hex');
+  return md5sum.digest("hex");
 }
